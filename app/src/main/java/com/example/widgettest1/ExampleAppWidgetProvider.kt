27ff082fb5b55c43b2fb5a6b2a5d9f59115ef8f6
usmon <usmonov11670122@gamil.com>
2023-07-05 17:13:40 +0500

    add_size_widget

--- app/src/main/java/com/example/widgettest1/ExampleAppWidgetProvider.kt
+++ app/src/main/java/com/example/widgettest1/ExampleAppWidgetProvider.kt
@@ -5,7 +5,10 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import android.widget.RemoteViews
+import android.widget.Toast
 
 
 class ExampleAppWidgetProvider : AppWidgetProvider() {
@@ -31,7 +34,51 @@ class ExampleAppWidgetProvider : AppWidgetProvider() {
             remoteViews.setOnClickPendingIntent(R.id.example_widget_button,pendingIntent)
             remoteViews.setCharSequence(R.id.example_widget_button,"setText",buttonText)
 
+
+            var appWidgetOptions = appWidgetManager.getAppWidgetOptions(appwidgetId)
+
+            resizeWidget(appWidgetOptions,remoteViews)
+
+            Toast.makeText(context, "onUpdate", Toast.LENGTH_SHORT).show()
+
             appWidgetManager.updateAppWidget(appwidgetId,remoteViews)
         }
     }
+
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+
+        var views  = RemoteViews(context!!.packageName,R.layout.example_app_widget_provider)
+
+        resizeWidget(newOptions!!,views)
+
+        appWidgetManager!!.updateAppWidget(appWidgetId,views)
+    }
+
+    private fun resizeWidget(appWidgetOptions:Bundle,views: RemoteViews){
+
+
+        var minWidth = appWidgetOptions!!.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+        var maxWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH)
+        var minHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
+        var maxHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT)
+
+        if (maxHeight > 150){
+            views.setViewVisibility(R.id.example_text_widget, View.VISIBLE)
+        }else{
+            views.setViewVisibility(R.id.example_text_widget,View.GONE)
+        }
+    }
+
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+        Toast.makeText(context, "onEnabled", Toast.LENGTH_SHORT).show()
+    }
+
 }
